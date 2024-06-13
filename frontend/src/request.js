996027b2e1d8c3ef60a6b62c881aa3effd58a4f5
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2024-06-13 19:36:19 +1000

    progress for lurk for work

---
+++ frontend/src/request.js
@@ -0,0 +1,97 @@
+import { useGo } from './route.js';
+import { useLoading, useMessage } from './utils.js';
+import { BACKEND_PORT } from './config.js';
+
+let baseURL = 'http://localhost:' + BACKEND_PORT;
+// let baseURL = "https://lurkforwork-backend-8z6r.onrender.com";
+
+// Encapsulate the logic of sending HTTP requests
+export default function request(url, params, config) {
+  if (!config?.hideLoading) useLoading();
+  const initConfigs = {
+    method: 'GET',
+    params: null,
+    body: null,
+    headers: {},
+    cache: 'no-cache',
+    responseType: 'JSON',
+  };
+  url = baseURL + url;
+  config = Object.assign(initConfigs, config);
+  let { method, responseType } = config;
+  config.method = method.toUpperCase();
+  // with token
+  let token = localStorage.getItem('token');
+  if (token) config.headers['Authorization'] = token;
+  config.headers['Content-Type'] = 'application/json';
+  config.headers['Accept'] = 'application/json';
+
+  let paramsRes = '';
+  if (/^(POST|PUT|PATCH|DELETE)$/i.test(config.method)) {
+    if (params != null) params = JSON.stringify(params);
+    config.body = params;
+  } else {
+    for (const key in params) {
+      const item = params[key];
+      paramsRes += `&${key}=${item}`;
+    }
+  }
+  // concat get url
+  if (paramsRes) url = url + `?${paramsRes}`;
+  // send fetch request
+  return fetch(url, config)
+    .then((response) => {
+      let { status, statusText } = response;
+      // judge response status
+      if (status >= 200 && status < 400) {
+        let result;
+        switch (responseType.toUpperCase()) {
+          case 'JSON':
+            result = response.json();
+            break;
+          case 'TEXT':
+            result = response.text();
+            break;
+          case 'BLOB':
+            result = response.blob();
+            break;
+          case 'ARRAYBUFFER':
+            result = response.arrayBuffer();
+            break;
+        }
+        return result;
+      }
+      return Promise.reject({
+        code: 'STATUS ERROR',
+        status,
+        response: response.json(),
+      });
+    })
+    .catch((reason) => {
+      if (reason && reason.code === 'STATUS ERROR') {
+        reason.response.then((res) => {
+          switch (reason.status) {
+            case 400:
+              useMessage('error', res.error);
+              break;
+            case 401:
+              useMessage('error', res.error);
+              break;
+            case 403:
+              useMessage('error', res.error);
+              useGo('#login');
+              break;
+            case 404:
+              useMessage('error', res.error);
+              break;
+          }
+        });
+      }
+      return Promise.reject(reason);
+    })
+    .finally(() => {
+      setTimeout(() => {
+        useLoading().hideLoading();
+      }, 500);
+    });
+}
